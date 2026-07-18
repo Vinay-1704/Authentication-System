b8486ecc1a544d9d2c313b06213644636e2f9756
Vinay <dasarinavya1704@gmail.com>
2026-07-19 00:20:59 +0530

    Use environment variable for database

--- Backend/main.py
+++ Backend/main.py
@@ -22,7 +22,8 @@ app= FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://authentication-system-pi-neon.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
