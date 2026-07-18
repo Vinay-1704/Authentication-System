dd56f9bbb702671445eb4a8c6e2e850faf546402
Vinay <dasarinavya1704@gmail.com>
2026-07-18 23:24:53 +0530

    Use environment variable for database

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url="postgresql://postgres:Vinay%401704@localhost:5432/auth_demo"
+
+# db_url="postgresql://postgres:Vinay%401704@localhost:5432/auth_demo"
+db_url=os.getenv("postgresql://neondb_owner:npg_P64alXrRmIBk@ep-gentle-darkness-awrr1byn.c-12.us-east-1.aws.neon.tech/neondb?sslmode=require")
 
 engine= create_engine(db_url)
 
